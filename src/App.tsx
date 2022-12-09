73b0d2b331808027a13754fe122b135752bf988e
Camatoziz <vova.higer1@gmail.com>
2022-12-09 23:19:48 +0300

    tasks-02

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,78 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const App=()=>{
-    const firstSubscriber=()=>{
-        console.log("Hello, I am Vasya!")
+import {Button} from './components/Button';
+import {NewComponent} from './NewComponent';
+/*
+const App = () => {
+    const Button1Foo=(subscriber: string)=>{
+        console.log(subscriber)
     }
-    return(
-        <div>
-            <button onClick={firstSubscriber}>YouTube-1</button>
-            {/*<button onClick={()=>{console.log("Hello!")}}>YouTube-2</button>*/}
+    const Button2Foo=(subscriber: string)=>{
+        console.log(subscriber)
+    }
+    const Button3Foo=()=>{
+        console.log("Im stupid button")
+    }
+    return (
+        <div className='App'>
+            <Button name={'YoutubeChanel-1'} callBack={()=>Button1Foo('Im Vasya')}/>
+            <Button name={'YoutubeChanel-2'} callBack={()=>Button2Foo('Im Ivan')}/>
+            <Button name={'WTF?'} callBack={Button3Foo}/>
+        </div>
+    )
+}*/
+/*
+const App = () => {
+    let [a, setA]=useState(0)
+
+    const onClickHandler=()=>{
+        setA(++a)
+        console.log(a)
+    }
+    const onClickHandler0=()=>{
+        setA(a=0)
+        console.log(a)
+    }
+    return (<div>
+            <h1>{a}</h1>
+        <button onClick={onClickHandler}>+1</button>
+        <button onClick={onClickHandler0}>0</button>
         </div>
     )
 }
+*/
+type FilterType = 'All' | 'RUBLS' | 'Dollars'
+const App = () => {
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+    const [filter, setFilter] = useState('All')
+    let currentMoney = money
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter(el => el.banknots === 'RUBLS')
+    }
+    if (filter === 'Dollars') {
+        currentMoney = money.filter(el => el.banknots === 'Dollars')
+    }
+    const Filter = (banknots: string) => {
+        setFilter(banknots)
+    }
 
 
-
-
-
-
-
+    return (
+        <div>
+            <NewComponent filteredArray={currentMoney} callBack={Filter}/>
+        </div>
+    )
+}
 
 /*import {Header} from './site/Header';
 import {Footer} from './site/Footer';
@@ -50,25 +103,25 @@ export default App;*/
         </div>
     )*/
 
-    /*const [students, setStudents] = useState([
-            {id: 1, name: "James", age: 8},
-            {id: 2, name: "Robert", age: 18},
-            {id: 3, name: "John", age: 28},
-            {id: 4, name: "Michael", age: 38},
-            {id: 5, name: "William", age: 48},
-            {id: 6, name: "David", age: 58},
-            {id: 7, name: "Richard", age: 68},
-            {id: 8, name: "Joseph", age: 78},
-            {id: 9, name: "Thomas", age: 88},
-            {id: 10, name: "Charles", age: 98},
-            {id: 11, name: "Christopher", age: 100},
-        ]
-    )
-    return (
-    <div>
-        <NewComponent students={students}/>
-    </div>
-    )*/
+/*const [students, setStudents] = useState([
+        {id: 1, name: "James", age: 8},
+        {id: 2, name: "Robert", age: 18},
+        {id: 3, name: "John", age: 28},
+        {id: 4, name: "Michael", age: 38},
+        {id: 5, name: "William", age: 48},
+        {id: 6, name: "David", age: 58},
+        {id: 7, name: "Richard", age: 68},
+        {id: 8, name: "Joseph", age: 78},
+        {id: 9, name: "Thomas", age: 88},
+        {id: 10, name: "Charles", age: 98},
+        {id: 11, name: "Christopher", age: 100},
+    ]
+)
+return (
+<div>
+    <NewComponent students={students}/>
+</div>
+)*/
 
 
 export default App;
---
+++ src/NewComponent.tsx
@@ -0,0 +1,33 @@
+import {Button} from './components/Button';
+import React from 'react';
+import app from './App';
+type NewComponentType = {
+    filteredArray: Array<NewComponentTypeArray>
+    callBack:()=>string
+}
+type NewComponentTypeArray = {
+    banknots: string
+    value: number
+    number: string
+}
+export const NewComponent = (props: NewComponentType) => {
+    const Filter = (banknots: string) => {
+        props.callBack()
+    }
+    return (
+        <div>
+            <ul>
+                {props.filteredArray.map((el, index) => {
+                    return <li key={index}>
+                        <span>{el.banknots}</span>
+                        <span>{el.value}</span>
+                        <span>{el.number}</span>
+                    </li>
+                })}
+            </ul>
+            <Button name={'All'} callBack={() => Filter('All')}/>
+            <Button name={'RUBLS'} callBack={() => Filter('RUBLS')}/>
+            <Button name={'Dollars'} callBack={() => Filter('Dollars')}/>
+        </div>
+    )
+}
